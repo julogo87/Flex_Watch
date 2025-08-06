018850c68e48d21de0db609b0d947df8c7322db7
julogo87 <julogo87@hotmail.com>
2025-08-06 11:59:26 -0500

    Add files via upload

---
+++ packages.sh
@@ -0,0 +1,5 @@
+# packages.sh
+
+# Este comando le dice a Streamlit que instale el navegador Firefox
+# Y también todas las dependencias del sistema operativo Linux que necesita para correr.
+playwright install --with-deps firefox
